482c9ba849f6186169d5bee781f06f6adbfbc8bf
Ahmad Bakare <65125857+adelekeahmad@users.noreply.github.com>
2022-12-25 19:51:46 +0100

    UI bootstrapping
    added semantic ui for bootstrapping

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import pic1 from './image/in.png';
+import pic2 from './image/whtsap.png';
+import pic3 from './image/ytb.png';
+import SingleComment from './SingleComment';
+import UserCard from './UserCard';
+const App = () => {
+    return(
+<div className='ui-comments'>
+    <UserCard>
+        <div>
+         Hello guys, my name is Ahmad and I live in NewYork.   
+        </div>
+    </UserCard>
+    <UserCard>
+    <SingleComment 
+    name='Bobby' 
+    date='Today at 5:PM' 
+    text='Hello G-Landers.'
+    picture={ pic1 }
+    />
+    </UserCard>
+    <UserCard>
+    <SingleComment 
+    name='Saint' 
+    date='Today at 6:PM' 
+    text='Hi G-Landee.'
+    picture={ pic2 }
+    />
+    </UserCard>
+    <UserCard>
+    <SingleComment 
+    name='Kay' 
+    date='Toady at 7:PM' 
+    text='We got you covered bro.'
+    picture={ pic3 }
+    />
+    </UserCard>
+</div>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    )
+}
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
